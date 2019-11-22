1e8f5ffd002336e3b082f96109553e0cddf15110
veronidas <veronabrkich@gmail.com>
2019-11-22 13:31:03 -0300

    boilerplate

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const fs = require("fs");
+const path = require("path");
+
 module.exports = () => {
   // ...
 };
+
+function showMessage() {
+  const message = "Hi!!";
+
+  for (let i = 0; i < 5; i++) {
+    console.log(message);
+  }
+}
